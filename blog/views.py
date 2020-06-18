401589c43a58876aca3751a70c0c6cf65536ef3e
Keith Turner <turner@keithturner.com>
2020-06-18 13:48:15 -0600

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+# REMOVE? from django.shortcuts import render
+
+from django.views.generic import ListView, DetailView # New
+
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView): # new
+    model = Post
+    template_name = 'post_detail.html'    

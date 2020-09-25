5fd9b2baa6b420b6ae5287eec8a775e055c7f68e
Keith Turner <turner@keithturner.com>
2020-09-25 14:58:54 -0600

    heroku config files and updates

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'ua40(n_b&k_%(t=b7lxmw&yyv*=&3gl^t#3jb^@$16+*nwb!!#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
